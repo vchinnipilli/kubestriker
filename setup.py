31fbecbc2344276617aee51d9ed02eb9e2e03a34
Vasant Chinnipilli <vasantkumarchinnipilli@gmail.com>
2021-01-14 23:41:39 +1100

    Added License

--- setup.py
+++ setup.py
@@ -7,18 +7,18 @@ with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
 setuptools.setup(
-    name="kubestrike",
+    name="kubestriker",
     version="v1.0.0",
     author="vasant chinnipilli",
     author_email="vchinnipilli@gmail.com.com",
     description="A Blazing fast Security Auditing tool for Kuberentes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/kubestrike",
+    url="https://github.com/vchinnipilli/kubestriker",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.0',
